50196d21d9183ec3b6bd7b61a3d611a2c0ee98de
Rudy-009 <rofxnaos@gmail.com>
2025-06-02 11:49:50 +0900

    feat: 더미데이터로 TableView 연결

--- Combine-With-UIKit/Weather/WeatherView.swift
+++ Combine-With-UIKit/Weather/WeatherView.swift
@@ -12,18 +12,30 @@ class WeatherView: UIView {
     
     public var label: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 36, weight: .bold)
+        label.font = .systemFont(ofSize: 24, weight: .bold)
         label.textColor = .label
+        label.text = "사용자 이름"
         return label
+    }()
+    
+    public var deleteDataButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "trash.circle"), for: .normal)
+        button.contentMode = .scaleAspectFill
+        return button
+    }()
+    
+    public var refreshButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "arrow.clockwise.circle"), for: .normal)
+        button.contentMode = .scaleAspectFill
+        return button
     }()
     
     public let tableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(WeatherForecastCell.self, forCellReuseIdentifier: WeatherForecastCell.identifier)
-        // 테이블 뷰의 rowHeight를 동적으로 설정하거나 고정 값으로 설정할 수 있습니다.
-        // tableView.rowHeight = UITableView.automaticDimension
-        // tableView.estimatedRowHeight = 100 // 예상 높이, automaticDimension과 함께 사용
         return tableView
     }()
     
@@ -35,16 +47,30 @@ class WeatherView: UIView {
     private func setup() {
         self.addSubview(label)
         self.addSubview(tableView)
+        self.addSubview(deleteDataButton)
+        self.addSubview(refreshButton)
         
         label.snp.makeConstraints { make in
-            make.height.equalTo(60)
-            make.leading.trailing.equalToSuperview().inset(20)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(30)
             make.top.equalToSuperview().offset(100)
+        }
+        
+        deleteDataButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-30)
+            make.centerY.equalTo(label.snp.centerY)
+            make.height.width.equalTo(50)
+        }
+        
+        refreshButton.snp.makeConstraints { make in
+            make.trailing.equalTo(deleteDataButton.snp.leading).inset(20)
+            make.centerY.equalTo(deleteDataButton)
+            make.height.width.equalTo(50)
         }
 
         tableView.snp.makeConstraints { make in
             make.top.equalTo(label.snp.bottom).offset(20)
-            make.leading.trailing.equalToSuperview().inset(-20)
+            make.leading.trailing.equalToSuperview().inset(10)
             make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom).offset(-20)
         }
     }
